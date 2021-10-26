f84dfaefbec61f9b2b1335cde6cb66aa1dc97ac7
John Uberbacher <johnuberbacher@gmail.com>
2021-10-25 21:57:29 -0600

    UI work

---
+++ src/components/body.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class Body extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    
+                </Row>
+            </Container>
+        )   
+    }
+}
+
+export default Body;
